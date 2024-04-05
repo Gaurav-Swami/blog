5870728299a2dcfac3f4054b3cac139b6d24d8c5
Gaurav Swami <gauravswami0404@gmail.com>
2024-04-05 22:53:14 +0530

    Created Header section with Links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
