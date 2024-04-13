5b86484fb94458f15fc7196848ac1ba4f84cd548
Gaurav Swami <gauravswami0404@gmail.com>
2024-04-13 16:06:37 +0530

    added functionality for storing user in database and loading animation with dealing of frontend validations

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
+  console.log("content recieved");
   if (
     !username ||
     !email ||
